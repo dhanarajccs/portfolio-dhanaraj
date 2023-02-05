1a927ff31548f59e54d1b9d91a244cab9f993475
dhanarajccs <dhanarajccs@gmail.com>
2023-02-05 12:53:37 +0530

    install typewriter-effect package

--- src/pages/banner/Banner.js
+++ src/pages/banner/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Lottie from 'lottie-react';
 import webAnimation from './../../assets/banner/19438-web-designer.json'
+import Typewriter from 'typewriter-effect';
 
 const Banner = () => {
   return (
@@ -8,15 +9,31 @@ const Banner = () => {
       <div className="bg-base-200 p-10">
         <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-5 gap-3">
           <div className="w-[300px] lg:w-full">
-           <Lottie animationData={webAnimation} loop />
+            <Lottie animationData={webAnimation} loop={true} />
           </div>
+
           <div className="w-[300px] lg:w-full flex flex-col justify-center items-enter">
-            <h1 className="lg:text-5xl text-secondary font-bold">Dhanaraj Narasimmareddy!</h1>
+            <h1 className="lg:text-5xl text-secondary font-bold">             
+              <Typewriter
+                options={{
+                  strings: ["I'm Dhanaraj Narasimmareddy!", "A Full Stack Web Developer"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h1>
             <p className="py-6 lg:text-lg text-sm text-center">
-                Hi, I'm Dhanaraj, a skilled and enthusiastic React web developer with 5 years of experience. I am passionate about creating intuitive, user-friendly, and visually appealing web applications that meet the needs of both businesses and users.
-            <br /><br />    
-                I have a strong foundation in React, HTML, CSS, JavaScript, and other relevant technologies. I take pride in writing clean, maintainable, and well-documented code, and I am always looking for ways to improve my skills and stay up-to-date with the latest trends in web development.
-            </p>            
+              Hi, I'm Dhanaraj, a skilled and enthusiastic React web developer
+              with 5 years of experience. I am passionate about creating
+              intuitive, user-friendly, and visually appealing web applications
+              that meet the needs of both businesses and users.
+              <br />
+              <br />I have a strong foundation in React, HTML, CSS, JavaScript,
+              and other relevant technologies. I take pride in writing clean,
+              maintainable, and well-documented code, and I am always looking
+              for ways to improve my skills and stay up-to-date with the latest
+              trends in web development.
+            </p>
           </div>
         </div>
       </div>
